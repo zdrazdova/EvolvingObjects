5e628fc360bb05ae1c9ac4653494c81067a6abb6
Zuzana Drazdova <zuzana.drazdova@sathea.cz>
2021-03-15 11:20:15 +0100

    Modulation continuation

---
+++ custom_geometry.py
@@ -0,0 +1,104 @@
+import math
+
+
+from sympy.geometry import Line, Ray, Point, Segment
+from sympy import pi, sin, cos
+
+
+class MyRay:
+
+    def __init__(self, origin, ray_angle, base_angle):
+        # Intensity is calculated according to Lambertian distribution
+        self.intensity = abs(math.sin(math.radians(abs(ray_angle-base_angle))))
+        # End coordinates are calculated from ray angle
+        x_coordinate = 10000 * math.cos(math.radians(ray_angle)) + origin.x
+        y_coordinate = 10000 * math.sin(math.radians(ray_angle)) + origin.y
+        # Ray goes from the origin in direction of end coordinates
+        self.ray = Ray(origin, Point(x_coordinate, y_coordinate))
+        # Array used for storing segments of reflected ray
+        self.ray_array = [Ray(origin, Point(x_coordinate, y_coordinate))]
+
+def compute_reflection(ray, surface, intersection):
+    #print(ray)
+    #print((surface))
+    #print("intersection" + str(intersection))
+    orig_point = ray.p1
+    parallel = surface.parallel_line(orig_point)
+    perpendicular = surface.perpendicular_line(intersection)
+    meet_point = parallel.intersection(perpendicular)[0]
+    x_diff = (meet_point.x - orig_point.x)
+    y_diff = (meet_point.y - orig_point.y)
+    new_point = Point(meet_point.x + x_diff, meet_point.y + y_diff)
+    #print(intersection, new_point)
+    reflected_ray = Ray(intersection, new_point)
+    return reflected_ray
+
+
+# computes intersections of rays from LED and road below the lamp
+def compute_intersections(ind):
+    inter_array = []
+    intensity_sum = 0
+    for ray in ind.original_rays:
+        inter_point = ind.road.intersection(ray.ray_array[-1])
+        if inter_point != []:
+            intensity_sum += ray.intensity
+            inter_array.append(inter_point[0].x)
+    # print(len(ind.original_rays))
+    # print(inter_array)
+    ind.intersections_on = inter_array
+    ind.intersections_on_intensity = intensity_sum
+
+
+def compute_reflections_two_segments(ind):
+    ind.compute_right_segment()
+    ind.compute_left_segment()
+    no_of_reflections = 0
+    for ray in ind.original_rays:
+        #print("NEW ")
+        continue_left = True
+        continue_right = True
+        previous_i_r = Point(0, 0)
+        previous_i_l = Point(0, 0)
+        ray.ray_array=[ray.ray]
+        while continue_left or continue_right:
+            continue_left = False
+            continue_right = False
+            last_ray = ray.ray_array[-1]
+            intersection_r = last_ray.intersection(ind.right_segment)
+            #print("right ", len(intersection_r))
+
+            if intersection_r and intersection_r[0] != previous_i_r:
+                #ax.print_ray(last_ray)
+                #ax.print_point(intersection_r[0])
+                previous_i_r = intersection_r[0]
+                intersection = intersection_r[0]
+                reflected_ray = compute_reflection(last_ray, ind.right_segment, intersection)
+                no_of_reflections += 1
+                new_ray_array = ray.ray_array[:-1]
+                new_ray_array.append(Ray(last_ray.p1, intersection_r[0]))
+                new_ray_array.append(reflected_ray)
+                ray.ray_array = new_ray_array
+                continue_left = True
+            last_ray = ray.ray_array[-1]
+            intersection_l = last_ray.intersection(ind.left_segment)
+            #print("left ", len(intersection_l))
+            if intersection_l and intersection_l[0] != previous_i_l:
+                #ax.print_ray(last_ray)
+                #ax.print_point(intersection_l[0])
+                previous_i_l = intersection_l[0]
+                intersection = intersection_l[0]
+                reflected_ray = compute_reflection(last_ray, ind.left_segment, intersection)
+                no_of_reflections += 1
+                new_ray_array = ray.ray_array[:-1]
+                new_ray_array.append(Ray(last_ray.p1, intersection_l[0]))
+                new_ray_array.append(reflected_ray)
+                ray.ray_array = new_ray_array
+                continue_right = True
+    ind.no_of_reflections = no_of_reflections
+
+
+def prepare_intersections(points):
+    x_coordinates = []
+    for point in points:
+        x_coordinates.append(float(point))
+    return sorted(x_coordinates)
--- evolution.py
+++ evolution.py
@@ -1,9 +1,7 @@
-import json
 import random
-import copy as cp
 import math
-import sys
 
+import custom_geometry as cg
 import custom_operators as co
 import auxiliary as ax
 
@@ -12,23 +10,8 @@ from deap import creator
 from deap import tools
 from deap.tools import HallOfFame
 from python_json_config import ConfigBuilder
-
 from sympy.geometry import Line, Ray, Point, Segment
 from sympy import pi, sin, cos
-
-
-class MyRay:
-
-    def __init__(self, origin, ray_angle, base_angle):
-        # Intensity is calculated according to Lambertian distribution
-        self.intensity = abs(math.sin(math.radians(abs(ray_angle-base_angle))))
-        # End coordinates are calculated from ray angle
-        x_coordinate = 10000 * math.cos(math.radians(ray_angle)) + origin.x
-        y_coordinate = 10000 * math.sin(math.radians(ray_angle)) + origin.y
-        # Ray goes from the origin in direction of end coordinates
-        self.ray = Ray(origin, Point(x_coordinate, y_coordinate))
-        # Array used for storing segments of reflected ray
-        self.ray_array = [Ray(origin, Point(x_coordinate, y_coordinate))]
 
 
 class Individual:
@@ -87,7 +70,7 @@ class Individual:
                 angle = 180 + ray*step + step/2 + base_angle
             else:
                 angle = random.randint(180, 360) + base_angle
-            new_ray = MyRay(self.origin, angle, base_angle)
+            new_ray = cg.MyRay(self.origin, angle, base_angle)
             ray_array.append(new_ray)
         return ray_array
 
@@ -127,96 +110,12 @@ class Individual:
         self.left_segment = Segment(base_left_p, Point(float(left_end_x),float(left_end_y)))
 
 
-def compute_reflection(ray, surface, intersection):
-    #print(ray)
-    #print((surface))
-    #print("intersection" + str(intersection))
-    orig_point = ray.p1
-    parallel = surface.parallel_line(orig_point)
-    perpendicular = surface.perpendicular_line(intersection)
-    meet_point = parallel.intersection(perpendicular)[0]
-    x_diff = (meet_point.x - orig_point.x)
-    y_diff = (meet_point.y - orig_point.y)
-    new_point = Point(meet_point.x + x_diff, meet_point.y + y_diff)
-    #print(intersection, new_point)
-    reflected_ray = Ray(intersection, new_point)
-    return reflected_ray
-
-
-# computes intersections of rays from LED and road below the lamp
-def compute_intersections(ind):
-    inter_array = []
-    intensity_sum = 0
-    for ray in ind.original_rays:
-        inter_point = ind.road.intersection(ray.ray_array[-1])
-        if inter_point != []:
-            intensity_sum += ray.intensity
-            inter_array.append(inter_point[0].x)
-    # print(len(ind.original_rays))
-    # print(inter_array)
-    ind.intersections_on = inter_array
-    ind.intersections_on_intensity = intensity_sum
-
-
-def compute_reflections_two_segments(ind):
-    ind.compute_right_segment()
-    ind.compute_left_segment()
-    no_of_reflections = 0
-    for ray in ind.original_rays:
-        #print("NEW ")
-        continue_left = True
-        continue_right = True
-        previous_i_r = Point(0, 0)
-        previous_i_l = Point(0, 0)
-        ray.ray_array=[ray.ray]
-        while continue_left or continue_right:
-            continue_left = False
-            continue_right = False
-            last_ray = ray.ray_array[-1]
-            intersection_r = last_ray.intersection(ind.right_segment)
-            #print("right ", len(intersection_r))
-
-            if intersection_r and intersection_r[0] != previous_i_r:
-                #ax.print_ray(last_ray)
-                #ax.print_point(intersection_r[0])
-                previous_i_r = intersection_r[0]
-                intersection = intersection_r[0]
-                reflected_ray = compute_reflection(last_ray, ind.right_segment, intersection)
-                no_of_reflections += 1
-                new_ray_array = ray.ray_array[:-1]
-                new_ray_array.append(Ray(last_ray.p1, intersection_r[0]))
-                new_ray_array.append(reflected_ray)
-                ray.ray_array = new_ray_array
-                continue_left = True
-            last_ray = ray.ray_array[-1]
-            intersection_l = last_ray.intersection(ind.left_segment)
-            #print("left ", len(intersection_l))
-            if intersection_l and intersection_l[0] != previous_i_l:
-                #ax.print_ray(last_ray)
-                #ax.print_point(intersection_l[0])
-                previous_i_l = intersection_l[0]
-                intersection = intersection_l[0]
-                reflected_ray = compute_reflection(last_ray, ind.left_segment, intersection)
-                no_of_reflections += 1
-                new_ray_array = ray.ray_array[:-1]
-                new_ray_array.append(Ray(last_ray.p1, intersection_l[0]))
-                new_ray_array.append(reflected_ray)
-                ray.ray_array = new_ray_array
-                continue_right = True
-    ind.no_of_reflections = no_of_reflections
-
-
-def prepare_intersections(points):
-    x_coordinates = []
-    for point in points:
-        x_coordinates.append(float(point))
-    return sorted(x_coordinates)
 
 
 def compute_road_segments(ind):
     segments_size = ind.road_length / config.road.sections
     segments_intensity = [0] * config.road.sections
-    intersections = prepare_intersections(ind.intersections_on)
+    intersections = cg.prepare_intersections(ind.intersections_on)
     #print(intersections)
     left_border = ind.start
     right_border = ind.start
@@ -240,13 +139,9 @@ def compute_road_segments(ind):
     #print(segments_intensity_proportional)
 
 
-def evaluate_simple(individual):
-    #print("SIMPLE")
-    compute_reflections_two_segments(individual)
-    compute_intersections(individual)
-    #print(individual.intersections_on_intensity)
-    #print(len(individual.intersections_on))
-    #print(individual.intersections_on)
+def evaluate(individual):
+    cg.compute_reflections_two_segments(individual)
+    cg.compute_intersections(individual)
     compute_road_segments(individual)
     return len(individual.intersections_on)
 
@@ -257,9 +152,7 @@ def evolution():
     # Initiating evolutionary algorithm
     creator.create("Fitness", base.Fitness, weights=(1.0))
     creator.create("Individual", list, fitness=creator.Fitness)
-
     toolbox = base.Toolbox()
-
     toolbox.register("population", tools.initRepeat, list, Individual)
     toolbox.register("select", tools.selTournament, tournsize=2)
 
@@ -272,9 +165,9 @@ def evolution():
     mut_length_prob = config.evolution.operators.mutation.length_mutation_prob
 
     # Initiating elitism
-    #hof = HallOfFame(1)
+    hof = HallOfFame(1)
 
-    fitnesses = list(map(evaluate_simple, pop))
+    fitnesses = list(map(evaluate, pop))
     for ind, fit in zip(pop, fitnesses):
         ind.fitness = fit
     print("Start of evolution")
@@ -293,7 +186,6 @@ def evolution():
     # Begin the evolution
     for g in range(config.evolution.number_of_generations):
         # A new generation
-
         print("-- Generation %i --" % g)
 
         # Select the next generation individuals
@@ -303,7 +195,6 @@ def evolution():
 
         # Apply crossover and mutation on the offspring
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
-
             # cross two individuals with probability xover_prob
             if random.random() < xover_prob:
                 co.mate(child1, child2)
@@ -312,31 +203,27 @@ def evolution():
                 child2.fitness = 0
 
         for mutant in offspring:
-
             # mutate an individual with probability mut_angle_prob and mut_length_prob
             if random.random() < mut_angle_prob:
                 co.mutate_angle(mutant)
                 mutant.fitness = 0
-
             if random.random() < mut_length_prob:
                 co.mutate_length(mutant)
                 mutant.fitness = 0
 
         # Evaluate the individuals with an invalid fitness
         invalid_ind = [ind for ind in offspring if ind.fitness == 0]
-        fitnesses = map(evaluate_simple, invalid_ind)
+        fitnesses = map(evaluate, invalid_ind)
         for ind, fit in zip(invalid_ind, fitnesses):
             ind.fitness = fit
 
         print("  Evaluated %i individuals" % len(invalid_ind))
         # The population is entirely replaced by the offspring
         pop[:] = offspring
-        # hof.update(pop)
-
-        # hof.update(pop)
+        hof.update(pop)
 
         best_ind = tools.selBest(pop, 1)[0]
-        # best_ind = hof[0]
+        best_ind = hof[0]
 
         print("Best individual is %s, %s, %s, %s, %s, %s, %s" % (best_ind.left_length_coef, best_ind.left_angle,
                                                          best_ind.intersections_on_intensity, best_ind.fitness, ind.no_of_reflections,
@@ -344,12 +231,7 @@ def evolution():
         ax.draw(best_ind,"best"+str(g), config)
 
     print("-- End of (successful) evolution --")
-
-    best_ind = tools.selBest(pop, 1)[0]
     print("--")
-
-
-    print("Best individual is %s" % (best_ind.fitness))
 
 
 # create config parser
@@ -359,10 +241,7 @@ builder = ConfigBuilder()
 config = builder.parse_config('parameters.json')
 
 
-def main():
+if __name__ == "__main__":
+    random.seed("seed")
     # Run evolution algorithm
     evolution()
-
-
-if __name__ == "__main__":
-    main()
